5b14cf5e790682b37efb8b6e74b58be375647987
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-09-26 21:08:11 +0200

    made a folder for the models and added a class student to it

---
+++ Model/Student.cs
@@ -0,0 +1,8 @@
+﻿namespace AspNetCoreWeb.Model;
+
+public class Student
+{
+    public int Id { get; set; } = 0;
+    public string Name { get; set; } = string.Empty;
+    public int Roll { get; set; } = 0;
+}
